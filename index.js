4541213e1519f0d7c579e426a39fec0bb7e4aecf
Daniel J. Furman <daniel@knwfg.com>
2015-12-07 16:57:13 -0500

    Setup of NodeJS server for listener.

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ var app = require('express')();
 
 var server = require('http').Server(app);
 
+var io = require('socket.io')(server);
+
 server.listen(3000);
 
 app.get('/', function(request, response){
-	response.send('Hello World');
+	response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('A connection was made.');
 });
